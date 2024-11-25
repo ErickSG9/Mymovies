f8186f39b5f2fa3a432efcb46df4e63fa4776026
Erick Samano <l20211843@tectijuana.edu.mx>
2024-11-25 22:27:43 +0000

    Cambios de users

--- mymovies/settings.py
+++ mymovies/settings.py
@@ -35,6 +35,8 @@ print(ALLOWED_HOSTS)
 # Application definition
 
 INSTALLED_APPS = [
+    'movies.apps.MoviesConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATICFILES_DIRS =[
+     BASE_DIR / "static",
+    "/home/ubuntu/ProgWeb/Tutoriales/css/"
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
